dfc0869885e96df7f6b5fb11faea6bb93155dc5c
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-05-07 13:17:43 +0200

    added interval for facts

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../styles/facts.scss";
 
 const Facts = () => {
   const [fact, setFact] = useState("");
+
+  useEffect(() => {
+    getFact();
+    const interval = setInterval(() => {
+      getFact();
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const getFact = async () => {
     const urlAPI = "https://api.chucknorris.io/jokes/random";
