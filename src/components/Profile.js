397e3fb609c40b22322d0cb9c5082fa3e8f8a610
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-05-09 18:48:15 +0200

    started profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/profile.scss";
 
 const Profile = () => {
-  return <div className="profile">Profiledasdasda</div>;
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("name"));
+    if (items) {
+      setName(items);
+    }
+  }, []);
+  return (
+    <div className="profile">
+      <div className="profile_cover">
+        <p className="profile_coverName"> {name}</p>
+        <p className="profile_coverInfo">San Francisco, CA, Unated States</p>
+      </div>
+      <div className="profile_info">
+        <div className="profile_infoCard">
+          <img
+            className="profile_infoCardImg"
+            src="https://source.unsplash.com/random/500%C3%97500/?portrait"
+            alt=""
+          />
+          <p>hi</p>
+        </div>
+      </div>
+      {/* <div className="profile_info">
+        <div className="profile_infoCard">
+          <img
+            className="profile_infoCardImg"
+            src="https://source.unsplash.com/random/500%C3%97500/?portrait"
+            alt=""
+          />
+        </div>
+      </div> */}
+    </div>
+  );
 };
 
 export default Profile;
