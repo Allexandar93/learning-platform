f60f57435f6cce39f582a5bd95126a9ce850c25a
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-05-10 18:21:21 +0200

    changes to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,28 +13,16 @@ const Profile = () => {
   return (
     <div className="profile">
       <div className="profile_cover">
-        <p className="profile_coverName"> {name}</p>
-        <p className="profile_coverInfo">San Francisco, CA, Unated States</p>
-      </div>
-      <div className="profile_info">
-        <div className="profile_infoCard">
-          <img
-            className="profile_infoCardImg"
-            src="https://source.unsplash.com/random/500%C3%97500/?portrait"
-            alt=""
-          />
-          <p>hi</p>
+        <img
+          className="profile_coverImg"
+          src="https://source.unsplash.com/random/500%C3%97500/?portrait"
+          alt=""
+        />
+        <div className="profile_coverInfo">
+          <p className="profile_coverName"> {name}</p>
+          <p className="profile_coverInfo">San Francisco, CA, United States</p>
         </div>
       </div>
-      {/* <div className="profile_info">
-        <div className="profile_infoCard">
-          <img
-            className="profile_infoCardImg"
-            src="https://source.unsplash.com/random/500%C3%97500/?portrait"
-            alt=""
-          />
-        </div>
-      </div> */}
     </div>
   );
 };
